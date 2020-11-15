09a33c3882ecfd765080daa70f06a2da2ca25106
Andre De Santana <ads.andredesantana@gmail.com>
2020-11-15 11:56:37 -0500

    Adding the Intervention Form and Controller

--- app/controllers/interventions_controller.rb
+++ app/controllers/interventions_controller.rb
@@ -1,18 +1,18 @@
 class InterventionsController < ApplicationController
 end
-
+ 
 private
 def createIntervention  
-
+ 
     Intervention.create(
         author: Employee.where(user_id: current_user.id).first.id,
-        customer_id: params[:selectcustomer],
-        building_id: params[:selectbuilding],
-        battery_id: params[:selectbattery],
-        column_id: params[:selectcolumn],
-        report: params[:selectdescription],
-        employee_id: params[:selectemployee],
-        elevator_id: params[:selectelevator])
+        customer_id: params[:sel_customer],
+        building_id: params[:sel_building],
+        battery_id: params[:sel_battery],
+        column_id: params[:sel_column],
+        report: params[:sel_description],
+        employee_id: params[:sel_employee],
+        elevator_id: params[:sel_elevator])
         
         redirect_to '/', notice: "Intervention created successfully"
     
