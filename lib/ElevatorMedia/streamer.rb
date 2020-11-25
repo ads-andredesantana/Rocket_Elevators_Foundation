563802f0f5e38be5aaded675835add4ae8580e8a
Andre De Santana <ads.andredesantana@gmail.com>
2020-11-25 11:32:40 -0500

    Test passed as expected, next => Check if Open Weather sends a response

--- lib/ElevatorMedia/streamer.rb
+++ lib/ElevatorMedia/streamer.rb
@@ -11,7 +11,11 @@ module ElevatorMedia
 
         end
 
-
+        # Method to get the weather of Montreal
+        def getWeather
+            options = { units: "metric", APPID: "b7057c0b99msh20e0b9062f32751p190fcbjsn0a91062986f5" }
+            OpenWeather::Current.city_id(6077243, options)
+        end
 
 
 
