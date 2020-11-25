52727e71b3f00a6be62ebfa6fe466764750b9e74
Andre De Santana <ads.andredesantana@gmail.com>
2020-11-25 10:59:54 -0500

    Test ok for initializing environment, next => Add OpenWeather gem and test the getContent function

--- spec/streamer_spec.rb
+++ spec/streamer_spec.rb
@@ -5,5 +5,14 @@ require 'ElevatorMedia/streamer'
 describe ElevatorMedia: :Streamer, :type => :feature do
     let!(:streamer){ElevatorMedia::Streamer.new}
 
+    # Testing the environment
+    it "a first test to initialize environment" do
+        expect(true).to be true
+        
+    end
+
+
+
+
 
 end
