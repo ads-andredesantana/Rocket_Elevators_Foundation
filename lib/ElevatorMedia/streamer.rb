bd4c2e432c77640a7e1ec84348a785cc16d25f24
Andre De Santana <ads.andredesantana@gmail.com>
2020-11-25 11:24:25 -0500

    Test failed as expected, next => Creating the getWeather method

--- lib/ElevatorMedia/streamer.rb
+++ lib/ElevatorMedia/streamer.rb
@@ -5,6 +5,9 @@ module ElevatorMedia
     class Streamer
 
         def getContent(type='weather')
+            obj =  self.getWeather
+            html="<div class='elevator-media-streamer-content'>#{obj}</div>"  
+            return html
 
         end
 
