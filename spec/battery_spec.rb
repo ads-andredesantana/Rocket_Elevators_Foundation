d4315206183d961f2d0836e1b6c16a1cad9752ab
Andre De Santana <ads.andredesantana@gmail.com>
2020-11-26 15:56:42 -0500

    Describing associations within battery

---
+++ spec/battery_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+
+RSpec.describe "Unit Tests to Battery Class" do
+
+    before do
+        @battery = Battery.new
+    end
+
+
+    context "battery object validation" do
+        it "Validates battery object" do
+            expect(@battery).to be_a(Battery)
+        end
+    end
+
+
+    describe Battery do
+        describe "Associations" do
+
+            
+
+        end 
+    end
+
+
+end
